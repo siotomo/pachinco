862404db1ebc6c67bcb8e981fd884722430457e0
shiomi tomoaki <403@keio.jp>
2020-08-31 23:00:25 +0900

    upadte gemfile for Heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/', to: 'article#index'
+  get 'articles/index', to: 'article#index'
 end
